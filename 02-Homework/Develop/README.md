503175c1cdcab84ee336ad3a44a1b36a956c48f8
timelessmoment05 <timelessmoment05@gmail.com>
2022-02-18 10:01:54 -0800

    Readme Generator code with npm installs, and tested README

---
+++ 02-Homework/Develop/README.md
@@ -0,0 +1,59 @@
+# README Generator
+  
+  ## Application Description
+
+  Create a README.md file based on user input
+
+  ## Table of Contents
+
+  * [Installation](#install)
+
+  * [User Story](#userStory)
+
+  * [Tests](#tests)
+
+  * [License](#license)
+
+  * [Contributors](#contributors)
+
+  * [Questions](#questions)
+
+
+
+  ## Installation
+
+  Run the following commands to install any necessary dependencies:
+
+  ```
+  npm i
+  ```
+
+  ## User Story
+
+  The user will install the README by doing the following: bash, node index.js, and npm i 
+
+  ## Tests
+
+  Run any applicable tests using the commands below:
+
+  ```
+  npm test
+  ```
+
+  ## Licensing
+
+  ![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## License
+
+This project is licensed under the MIT license.
+  
+  ## Contributors
+
+  Joanna Pastcan (2022)
+
+  ## Questions
+
+  Any questions regarding this repository can be directed to timelessmoment05@gmail.com.
+  View more work from this developer at [Jo37973](https://github.com/Jo37973/)
+
+  
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,20 +1,90 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `* [License](#license)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Application Description
 
-`;
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#install)
+
+  * [User Story](#userStory)
+
+  * [Tests](#tests)
+
+  ${renderLicenseLink(data.license)}
+
+  * [Contributors](#contributors)
+
+  * [Questions](#questions)
+
+
+
+  ## Installation
+
+  Run the following commands to install any necessary dependencies:
+
+  \`\`\`
+  ${data.install}
+  \`\`\`
+
+  ## User Story
+
+  ${data.userStory}
+
+  ## Tests
+
+  Run any applicable tests using the commands below:
+
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+
+  ## Licensing
+
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributors
+
+  ${data.contributors}
+
+  ## Questions
+
+  Any questions regarding this repository can be directed to ${data.email}.
+  View more work from this developer at [${data.github}](https://github.com/${data.github}/)
+
+  `;
 }
 
 module.exports = generateMarkdown;
